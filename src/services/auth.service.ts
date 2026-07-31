b92639ce8c65197c683b97b8cc4045b0982b3b62
shadab-ansari-aiits <shadabaits@gmail.com>
2026-07-31 16:35:54 +0530

    api files changes

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,23 +1,9 @@
-import { ILoginRequest } from "interfaces/auth";
+import type { ILoginRequest } from "../interfaces/auth";
 import client from "../utils/ApiClient";
 import API_URLS from "./endPoints";
 
 async function login(req: ILoginRequest): Promise<any> {
-  console.log("API_URLS.LOGIN", API_URLS.LOGIN);
   return client.post(API_URLS.LOGIN, req);
 }
 
-async function signup(req: any): Promise<any> {
-    console.log("API_URLS.SIGNUP", API_URLS.SIGNUP);
-  return client.post(API_URLS.SIGNUP, req);
-}
-
-// const logout = () => {
-//   localStorage.removeItem("user");
-// };
-
-export {
-    //   logout,
-  login,
-  signup
-};
+export { login };
